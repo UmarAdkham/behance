116b5f3f90eb9e851134985fb600e206368eef85
FaraJM21 <farruxrihsiyev@gmail.com>
2022-09-24 09:18:33 +0500

    Update App.tsx
    add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Register from './components/Register';
+
 
 function App() {
   return (
-    <div>App</div>
+    <div className="App">
+
+      <h1>Appda</h1>
+    </div>
   )
 }
 
