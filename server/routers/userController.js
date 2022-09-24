6bd95d76649bcfc850e5327199d49e3b20959ca9
FaraJM21 <farruxrihsiyev@gmail.com>
2022-09-24 12:17:23 +0500

    Login API qo'shildi

--- server/routers/userController.js
+++ server/routers/userController.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { register } = require('../controller/registerController');
+const { login, register } = require('../controller/loginController');
+
 
 const userRouter = express.Router()
 
 
 //BU REGISTER KANTROLLERI
-userRouter.post("/", register);
+userRouter.post("/register", register);
+
+/**
+ * LOGIN KANTROLLER
+ */
+userRouter.post('/login', login)
+
 
 
 
