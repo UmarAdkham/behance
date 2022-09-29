2b436650de87772deee34d9998eef428f1ab3c7b
Sanjarbek Urolov <107649644+sanjarbekurolov1121@users.noreply.github.com>
2022-09-28 22:29:34 -0700

    Diqqat css da yangilanish

--- client/src/routes/AccountPage.tsx
+++ client/src/routes/AccountPage.tsx
@@ -9,7 +9,7 @@ function AccountPage() {
     // let useref=useRef<HTMLInputElement>(null)
     // useref.current.src = URL.createObjectURL(event.target.files[0]);
   };
-  let vaqt=new Date().toLocaleString();
+  let vaqt = new Date().toLocaleString();
   return (
     <div className="account-page">
       <div className="add-photo-banner">
@@ -32,63 +32,50 @@ function AccountPage() {
           <h2>Добавить изображение баннера.</h2>
           <h4>Оптимальный размеры 3200 x 410px</h4>
         </div>
-        
       </div>
-    
+
 
       <div className="user-data">
         <div className="user-data-ichki-div">
+            <div className="user-photo-profile">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"
+                alt="404-user-photo"
+              />
+            </div>
 
-          <div className="user-photo-profile">
-            <img
-              src="https://i.mycdn.me/i?r=AyH4iRPQ2q0otWIFepML2LxR64uyGyLI8KN82xvbGA9UXg"
-              alt="404-user-photo"
-            />
-        <div className="user-photo-profile">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png"
-            alt="404-user-photo"
-          />
-        </div>
-        <div className="h2pb">
-          <h2>Sanjarbek Urolov</h2>
-          <p>Uzbekistan</p>
-          <button>
-            <h3>Редактировать профиль</h3>
-          </button>
-          <div className="adobe-portfolio">
-            <h3>Опробовать Adobe Portfolio</h3>
+            <div className="h2pb">
+              <h2>Sanjarbek Urolov</h2>
+              <p>Uzbekistan</p>
+              <button>
+                <h3>Редактировать профиль</h3>
+              </button>
+
+              <div className="adobe-portfolio">
+                <h3>Опробовать Adobe Portfolio</h3>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="container-for-uploading-images">
+            <div className="nav-bar"></div>
+            <div className="images-container"></div>
+          </div>
+          
+          <div className="nanyat">
+            <h3>Nanyat</h3>
+            <hr style={{ width: "285px" }} />
+            <h4>Ищете возможности?</h4>
+            <p>
+              Добавьте новый раздел «Нанять», чтобы сообщить посетителям, что вы
+              ищете новые возможности для обеспечения занятости.
+            </p>
           </div>
         </div>
+        <Uploat />
       </div>
-
-      <div className="container-for-uploading-images">
-
-       <div className="nav-bar">
-         {/* <Account_app/> */}
-       </div>
-       <div className="images-container">
-
-       </div>
-      </div>
-
-
-        <div className="nanyat">
-          <h3>Nanyat</h3>
-          <hr style={{ width: "285px" }} />
-          <h4>Ищете возможности?</h4>
-          <p>
-            Добавьте новый раздел «Нанять», чтобы сообщить посетителям, что вы
-            ищете новые возможности для обеспечения занятости.
-          </p>
-        </div>
-      </div>
-      <Uploat/>
-    </div>
-    </div>
   );
 }
 
 export default AccountPage;
-
-
