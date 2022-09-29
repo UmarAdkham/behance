f68af6e926353726413574f1efdde83d943a1bff
Abdulaziz2003333 <108785546+Abdulaziz2003333@users.noreply.github.com>
2022-09-29 11:59:04 +0500

    Update Login.tsx

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios'
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../hook/hook";
