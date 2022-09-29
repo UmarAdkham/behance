3644a06ce328258da4f349d475365f468b5cd1d5
Abdulaziz2003333 <108785546+Abdulaziz2003333@users.noreply.github.com>
2022-09-29 10:39:22 +0500

    Main qoshildi barcha fayllari blan

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AccountPage from "./router/AccountPage";
 import Login from "./router/Login";
+import Main from "./router/Main";
 import Register from "./router/Register";
-import './style/account-page.scss'
+import "./style/account-page.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} ></Route>
-        <Route path="register" element={<Register />} ></Route>
-        <Route path="accountPage" element={<AccountPage />} ></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="accountPage" element={<AccountPage />}></Route>
+        <Route path="main" element={<Main />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/router/Login.tsx
+++ client/src/router/Login.tsx
@@ -1,63 +1,67 @@
- import React, { useState } from "react";
+import React, { useState } from "react";
 import { loginInterface } from "../interface/registerInterface";
-import '../style/login.scss'
+import "../style/login.scss";
 
 function Login() {
-    const [user, setUser] = useState<loginInterface>({
-        email: "",
-        password: ""
-    });
+  const [user, setUser] = useState<loginInterface>({
+    email: "",
+    password: "",
+  });
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setUser({ email: "", password: "" });
-    };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setUser({ email: "", password: "" });
+  };
 
-    if (user.email === "admin@gmail.com" && user.password === "1234") {
-        // navigate('/main')
-        console.log('working');
-    }
+  if (user.email === "admin@gmail.com" && user.password === "1234") {
+    // navigate('/main')
+    console.log("working");
+  }
 
-    return (
-        <>
-            <div className="login-wrapper">
-                <div className="login-box">
-                    <div>
-                        <h1><span>Be</span> Behance</h1>
-                    </div>
-                    <div className="login-container">
-                        <form onSubmit={handleSubmit}>
-                            <h2>Login</h2>
-                            <p className="btn-socialMedia btn-google">Continue with Google</p>
-                            <p className="btn-socialMedia btn-facebook">Continue with Facebook</p>
-                            <p className="btn-socialMedia btn-apple">Continue with Apple</p>
-                            <p id="hr">-----------------or-------------------</p>
-                            <input
-                                type="email"
-                                name="email"
-                                value={user.email || ""}
-                                placeholder="Enter your email"
-                                onChange={handleChange}
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                value={user.password || ""}
-                                placeholder="Enter your password"
-                                onChange={handleChange}
-                            />
-                            <p className="link">Don't have an account yet ? Register</p>
-                            <button>Login</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="login-wrapper">
+        <div className="login-box">
+          <div>
+            <h1>
+              <span>Be</span> Behance
+            </h1>
+          </div>
+          <div className="login-container">
+            <form onSubmit={handleSubmit}>
+              <h2>Login</h2>
+              <p className="btn-socialMedia btn-google">Continue with Google</p>
+              <p className="btn-socialMedia btn-facebook">
+                Continue with Facebook
+              </p>
+              <p className="btn-socialMedia btn-apple">Continue with Apple</p>
+              <p id="hr">-----------------or-------------------</p>
+              <input
+                type="email"
+                name="email"
+                value={user.email || ""}
+                placeholder="Enter your email"
+                onChange={handleChange}
+              />
+              <input
+                type="password"
+                name="password"
+                value={user.password || ""}
+                placeholder="Enter your password"
+                onChange={handleChange}
+              />
+              <p className="link">Don't have an account yet ? Register</p>
+              <button>Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
