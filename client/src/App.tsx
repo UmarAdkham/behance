6c23abe0dfd58d057e8f005015ce2de7b7388c98
theyakuboff <yoqubovbaxtiyor208@gmail.com>
2022-10-04 10:35:01 +0500

    polvon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./routes/Login";
 import AccountPage from "./routes/AccountPage";
 import Register from "./routes/Register";
-
+import Main from "./routes/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} ></Route>
-        <Route path="register" element={<Register />} ></Route>
-        <Route path="accountPage" element={<AccountPage />} ></Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="accountPage" element={<AccountPage />}></Route>
+        <Route path="main" element={<Main />}></Route>
       </Routes>
     </BrowserRouter>
   );
