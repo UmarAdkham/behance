30559b272b66fe5cc10c583feaa4a18d6dc08562
Abu.akoo <tursunxojayevabdullox@gmail.com>
2022-10-04 10:49:03 +0500

    Update server

--- server/routers/PhotoViewsRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { views,  } = require("../controller/PhotoViewsController");
-
-const viewsRouter = express.Router();
-
-viewsRouter.put("/:id", views);
-
-
-module.exports = viewsRouter;
--- server/routers/PhotosRoutes.js
+++ server/routers/PhotosRoutes.js
@@ -1,11 +1,12 @@
 //
 const express = require("express")
-const {getPhotos, upload} = require("../controller/photosController")
+const { getPhotos, upload, views } = require("../controller/photosController")
 
 const photoRouter = express.Router()
 
-photoRouter.get("/photos" , getPhotos)
-photoRouter.get("/upload" , upload)
+photoRouter.get("/photos", getPhotos)
+photoRouter.get("/upload", upload)
+photoRouter.put("/:id", views);
 
 
 module.exports = photoRouter
