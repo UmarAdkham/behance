48c6a11826a3fa8ceb7f4079f75b75a34a5b65a4
Abdulaziz2003333 <108785546+Abdulaziz2003333@users.noreply.github.com>
2022-10-04 11:10:16 +0500

    Update Main

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import '../style/main.css'
+function Footer() {
+  return (
+   <div>
+     <div className="footer">
+        <ul>
+          <li>
+            {" "}
+            <a className="s3" href="#">
+              More Behance
+            </a>
+          </li>
+          <li>
+            {" "}
+            <select id="cars">
+              <option value="fr">French</option>
+              <option value="rus">Русский</option>
+              <option value="eng" selected>
+                English
+              </option>
+            </select>
+          </li>
+          <li>
+            {" "}
+            <a className="s4" href="#">
+              Livestreams
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a className="s4" href="#">
+              Hire
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a className="s4" href="#">
+              Jobs
+            </a>
+          </li>
+        </ul>
+      </div>
+   </div>
+  )
+}
+
+export default Footer
